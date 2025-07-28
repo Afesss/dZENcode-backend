ae7eadabc0d8f31fff15b18ef13a6b153e84a871
Afes <afesdaller@gmail.com>
2025-07-28 09:15:30 +0300

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("message", (msg) => {
+    console.log("Message received:", msg);
+    socket.broadcast.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
