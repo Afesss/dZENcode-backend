72f6b952ff4e13c819389227e7b6d05382dbdc41
Afes <afesdaller@gmail.com>
2025-07-28 09:25:29 +0300

    add cors

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://afesss.github.io/dZENcode"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },);
 
 let onlineUsers = 0;
 io.on("connection", (socket) => {
